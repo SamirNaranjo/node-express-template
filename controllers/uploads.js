5d1b3d3865da5f09162a30530fd294f9f009337c
samir <samirnaranjo1@gmail.com>
2023-08-28 22:28:22 -0300

    update controller upload file

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { response } = require("express")
+const { v4: uuidv4 } = require('uuid')
 
 
 const cargarArchivo = (req, res=response) => {
@@ -10,17 +11,28 @@ const cargarArchivo = (req, res=response) => {
     }
 
     const { archivo } = req.files;
+    const nombreCortado = archivo.namesplit('.');
+    const extension = nombreCortado [ nombreCortado - 1 ];
 
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+    // Validar extensiones
 
-    archivo.mv(uploadPath, (err) => {
+    const extensionesValidas = [ 'png','jpg','jpeg','gif'];
+    if ( !extensionesValidas.includes(extension) ) {
+        return res.status(400).json({
+            msg: `La extension ${ extension } no es permitida, ${ extensionesValidas}`
+        });
+    }
+
+    const nombreTemp = uuidv4() + '.' + extension;
+    const uploadPath = path.join(__dirname, '../uploads/', nombreTemp);
+
+    archivo.mv (uploadPath, (err) =>{
         if (err) {
-            return res.status(500).json({ err });
+            return res.status(500).json ({err});
         }
 
-        res.json({ msg: 'File uploaded to' + uploadPath });
-    })
-    
+        res.json({ msg: 'File uploaded to ' + uploadPath });
+    });
 
 }
 
